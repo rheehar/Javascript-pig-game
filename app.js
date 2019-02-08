46e1568f347520b9790c29491fa10ba1682fcd44
Fareedat <phariedah19@gmail.com>
2019-02-08 14:38:03 +0100

    First stage of pig game complete

--- app.js
+++ app.js
@@ -9,35 +9,96 @@ GAME RULES:
 
 */
 
-var scores, roundScores, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
+init();
 
-scores =[0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-  //1. Random number
-  var dice = Math.floor(Math.random() * 6) + 1;
-  //2. Display result
-  var diceDOM = document.querySelector('.dice');
-   diceDOM.style.display = 'block';
-   diceDOM.src = 'dice-' + dice + '.png';
+  if(gamePlaying){
+    //1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    //2. Display result
+    var diceDOM = document.querySelector('.dice');
+     diceDOM.style.display = 'block';
+     diceDOM.src = 'dice-' + dice + '.png';
 
-  //3. updatw the round score if the round number was not 1
+    //3. update the round score if the round number was not 1
+     if (dice !== 1){
+       //add score
+       roundScore += dice;
+       document.querySelector('#current-' + activePlayer).textContent = roundScore;
+     } else {
+       nextPlayer();
+     }
+  }
+
 });
 
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+  if(gamePlaying){
+    //add current score to global scores
+    scores[activePlayer] += roundScore;
+
+    //update UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  //check if player won the game
+  if (scores[activePlayer] >= 100){
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    gamePlaying = false;
+  } else {
+    //next player
+    nextPlayer();
+  }
+
+}
+
+});
+
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+
+
+
+function nextPlayer() {
+  //next player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+
+}
+
+function init() {
+  scores =[0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+
+  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('score-0').textContent = '0';
+  document.getElementById('score-1').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
 
 
 
@@ -52,6 +113,15 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 
 
+
+
+
+
+
+
+//state variable tells the condition of a system. we use it when we want to remember something
+//document.querySelector('.player-0-panel').classList.remove('active');
+//document.querySelector('.player-1-panel').classList.add('active');
 //console.log(dice);
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
